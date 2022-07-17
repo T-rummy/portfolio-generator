0f4ddf89cf17a2569d234e2a4fbd1cba88bb733b
Ruminert <ruminertt@tanners-air.attlocal.net>
2022-07-17 15:37:46 -0500

    generate-webpage

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-console.log(profileDataArgs);
+const [name, github] = profileDataArgs;
 
-const printProfileData  = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i+= 1){
-        console.log(profileDataArr[i]);
-    }
 
-    console.log('================');
+    fs.writeFile('index.html', generatePage(name, github), err => {
+        if (err) throw err;
 
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem);
-    })
-};
-
-printProfileData(profileDataArgs);
+        console.log('portfolio complete! checkout index.html to see the output!');
+    });
